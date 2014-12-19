1a9a427e669c5682f1c07691182c34e1d6a42b8b
cp9162 <cpollock@netdesignstudios.com>
2014-12-19 12:02:54 -0500

    add dashboard controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,9 @@ Rails.application.routes.draw do
   resources :tickets
 
   devise_for :users
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
   root 'welcome#index'
 
+  get 'dashboard' => 'dashboard#show'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
